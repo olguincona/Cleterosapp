81fd8450bbf7251238166f887cbad15a7a691397
Felipe Olguin <Folguinco@gmail.com>
2022-07-18 23:47:27 -0400

    Arreglos2

--- src/Components/ItemDetail.js
+++ src/Components/ItemDetail.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import ItemCount from './ItemCount';
 
 function ItemDetail({product}) {
   return (
     <>
       <h3>{product.title}</h3>
       <p>{product.price}</p>
+      <ItemCount />
     </>
   );
 }
